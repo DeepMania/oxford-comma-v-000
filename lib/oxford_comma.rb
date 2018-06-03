f01451b4bc289b06dfa3918c0b79ae32974d8b19
Yana Ustsinovich <yustinovich@icloud.com>
2018-06-03 03:03:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.size == 1
     array.join
-  elsif array.size == 2
+  elsif array.size == 2,3
     array.join(" and ")
   elsif array.size > 3
     array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
